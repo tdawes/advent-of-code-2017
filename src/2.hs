7625744db442553dcb53f5a0894c0c25e31e8b3b
Tom Dawes <tomdawes91@gmail.com>
2017-12-02 08:47:22 +0000

    2017: Day 2 (b)

--- src/2.hs
+++ src/2.hs
@@ -1,14 +1,24 @@
+import Data.List
+
 comparedElement :: (Num a) => [a] -> Int -> Int
 comparedElement list index = (index + (quot (length list) 2)) `mod` (length list)
 
 parseInput :: [Char] -> Int
 parseInput x = read x :: Int
 
-calculateValue :: (Num a, Ord a) => [a] -> a
-calculateValue line = abs $ (foldl1 max line) - (foldl1 min line)
+pairs :: [a] -> [(a, a)]
+pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
+
+sortElems :: (Ord a) => (a, a) -> (a, a)
+sortElems (x, y) = if x > y then (x, y) else (y, x)
+
+doDivision :: Maybe Int -> (Int, Int) -> Maybe Int
+doDivision a (x, y) = if (rem x y) == 0 then Just $ quot x y else a
+
+calculateValue :: [Int] -> Maybe Int
+calculateValue line = foldl doDivision Nothing $ map sortElems $ pairs line
 
 main = do
   lines <- map (map parseInput) <$> map words <$> lines <$> getContents
   let diffs = map calculateValue lines
-  let sum = foldl1 (+) diffs
-  print sum
+  print $ fmap sum $ sequence diffs
