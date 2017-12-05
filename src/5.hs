05629e7beda5ff3600eaa8cfe2501911f0e9769a
Tom Dawes <tomdawes91@gmail.com>
2017-12-05 14:24:26 +0000

    2017: Day 5 (b)

--- src/5.hs
+++ src/5.hs
@@ -1,24 +1,28 @@
+import qualified Data.Map as M
+
 parseInput :: [Char] -> Int
 parseInput x = read x :: Int
 
-type State = (Int, [Int])
+type State = (Int, M.Map Int Int)
 
-updateData :: [Int] -> Int -> [Int]
-updateData (d:ds) 0 = (d+1):ds
-updateData (d:ds) n = d:(updateData ds (n-1))
+step :: (Int -> Int) -> State -> State
+step update (p, d) = (newPosition, newData)
+  where v = d M.! p
+        newPosition = p + v
+        newData = M.insert p (update v) d
 
-step :: State -> State
-step (p, d) = (newPosition, newData)
-  where newPosition = p + (d !! p)
-        newData = updateData d p
-
-numSteps :: State -> Int
-numSteps (p, d) | p < 0 = 0
-                | p >= (length d) = 0
-                | otherwise = 1 + (numSteps $ step (p, d))
+numSteps :: (Int -> Int) -> M.Map Int Int -> Int
+numSteps update d = numSteps' (0, d) 0
+  where numSteps' (p, d) steps | p < 0 = steps
+                               | p >= M.size d = steps
+                               | otherwise = numSteps' (step update (p, d)) (steps + 1)
 
 main = do
   input <- map parseInput <$> lines <$> getContents
+  let mappedInput = M.fromList $ zip [0..length input] input
 
   -- Part A
-  print $ numSteps (0, input)
+  print $ numSteps (\d -> d + 1) mappedInput
+
+  -- Part B
+  print $ numSteps (\d -> if d >= 3 then d - 1 else d + 1) mappedInput
