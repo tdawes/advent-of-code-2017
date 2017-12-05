aa55429f85ebb3f1b6467e576d549b4d3a41d20a
Tom Dawes <tomdawes91@gmail.com>
2017-12-05 01:31:05 +0000

    2017: Clean-up Day 2

--- src/2.hs
+++ src/2.hs
@@ -1,24 +1,16 @@
 import Data.List
 
-comparedElement :: (Num a) => [a] -> Int -> Int
-comparedElement list index = (index + (quot (length list) 2)) `mod` (length list)
-
 parseInput :: [Char] -> Int
 parseInput x = read x :: Int
 
-pairs :: [a] -> [(a, a)]
-pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
-
-sortElems :: (Ord a) => (a, a) -> (a, a)
-sortElems (x, y) = if x > y then (x, y) else (y, x)
-
-doDivision :: Maybe Int -> (Int, Int) -> Maybe Int
-doDivision a (x, y) = if (rem x y) == 0 then Just $ quot x y else a
-
-calculateValue :: [Int] -> Maybe Int
-calculateValue line = foldl doDivision Nothing $ map sortElems $ pairs line
+pairs :: (Eq a) => [a] -> [(a, a)]
+pairs list = [ (x, y) | x <- list, y <- list, x /= y ]
 
 main = do
   lines <- map (map parseInput) <$> map words <$> lines <$> getContents
-  let diffs = map calculateValue lines
-  print $ fmap sum $ sequence diffs
+
+  -- Part A
+  print $ sum [ (maximum line) - (minimum line) | line <- lines ]
+
+  -- Part B
+  print $ sum [ quot x y | line <- lines, (x, y) <- pairs line, rem x y == 0 ]
