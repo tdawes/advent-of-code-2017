b70d2c0e2601b572c39f40f45e685deabaea34d8
Tom Dawes <tomdawes91@gmail.com>
2017-12-05 00:58:10 +0000

    2017: Day 3 (a) - 2nd attempt

--- src/3.hs
+++ src/3.hs
@@ -1,57 +1,34 @@
 import Data.Function.Memoize
 
+type Coord = (Int, Int)
+type Element = (Int, Coord)
+
 parseInput :: [Char] -> Int
 parseInput x = read x :: Int
 
-shift :: (Int, Int) -> (Int, Int) -> (Int, Int)
-shift (x, y) (c, d) = (x + c, y + d)
+shift :: Coord -> Coord -> Coord
+shift (x, y) (a, b) = (x + a, y + b)
 
-neighbours :: (Int, Int) -> [(Int, Int)]
-neighbours c = [ shift c (x, y) | x <- [-1..1], y <- [-1..1], x /= 0 || y /= 0 ]
+corners :: [Element]
+corners = [ (index, (i-1, i-1)) | i <- [1..], let index = (2*i - 1)^2 ]
 
-getPosition :: (Int, Int) -> Int
-getPosition (0, 0) = 1
-getPosition (x, y) | x + y > 0 && x == r = (l-1)^2 + (y + r)
-                   | x + y > 0 && y == r = (l-1)^2 + l + (r - x)
-                   | x + y <= 0 && x == -r = (l-1)^2 + 1 + (r - y)
-                   | otherwise = (l-1)^2 + l + (x + r)
-  where r = max (abs x) (abs y)
-        l = if x + y > 0 then 2 * r else 2 * r + 1
+nextCorner :: Int -> Element
+nextCorner index = head [ corner | corner <- corners, fst corner >= index ]
 
-smallerNeighbours :: (Int, Int) -> [(Int, Int)]
-smallerNeighbours (x, y) = [ n | n <- neighbours (x, y), getPosition n <= position ]
-  where position = getPosition (x, y)
+getCoordinate :: Int -> Coord
+getCoordinate i | i == s = c
+                | i >= s - d = (l - (s - i), l)
+                | i >= s - (2*d) = ((-l), l - (s - d - i))
+                | i >= s - (3*d) = ((s - (2*d) - i) - l, (-l))
+                | otherwise = (l, (s - (3*d) - i) - l)
+  where (s, c) = nextCorner i
+        l = fst c
+        d = 2*l
 
-calculateCoords :: Int -> (Int, Int)
-calculateCoords 1 = (0, 0)
-calculateCoords x = shift (calculateCoords (x-1)) delta
-  where delta = calculateDelta (x - 1)
-
-calculateDelta :: Int -> (Int, Int)
-calculateDelta 1 = (1, 0)
-calculateDelta x | (r `mod` 2 == 0 && x > r ^ 2 + r) = (1, 0)
-                 | (r `mod` 2 == 0 && x <= r ^ 2 + r) = (0, -1)
-                 | (r `mod` 2 == 1 && x > r ^ 2 + r) = (-1, 0)
-                 | (r `mod` 2 == 1 && x <=  r ^ 2 + r) = (0, 1)
-                 where r = findNearestSquareRoot (x - 1)
-
-findNearestSquareRoot :: Int -> Int
-findNearestSquareRoot x = floor $ sqrt $ fromIntegral x
-
-calculateWeightForCoords :: (Int, Int) -> Int
-calculateWeightForCoords' (0, 0) = 1
-calculateWeightForCoords' (x, y) = foldl1 (+) $ map calculateWeightForCoords $ smallerNeighbours (x, y)
-calculateWeightForCoords = memoize calculateWeightForCoords' 
-
-calculateWeight :: Int -> Int
-calculateWeight = calculateWeightForCoords . calculateCoords
-
-run :: Int -> Int -> Int
-run score x = if c > score then c else run score (x+1)
-    where c = calculateWeight x
+spiral = [ (i, getCoordinate i) | i <- [1..] ]
 
 main = do
   input <- parseInput <$> getContents
-  -- let input = 10
-  let score = run input 1
-  print score
+  -- Part A
+  let coord = getCoordinate input
+  print $ (abs $ fst coord) + (abs $ snd coord)
