cec47d373dcc140ae0440f16639717e3fd2dfc5a
Tom Dawes <tomdawes91@gmail.com>
2017-12-09 22:00:32 +0000

    2017: Day 9 (b)

--- src/9.hs
+++ src/9.hs
@@ -2,13 +2,13 @@
 removeIgnoredCharacters :: [Char] -> [Char]
 removeIgnoredCharacters [] = []
 removeIgnoredCharacters ('!':y:xs) = removeIgnoredCharacters xs
-removeIgnoredCharacters (x:xs) = [x] ++ removeIgnoredCharacters xs
+removeIgnoredCharacters (x:xs) = x:(removeIgnoredCharacters xs)
 
 removeGarbage :: [Char] -> [Char]
-removeGarbage x = removeGarbage' False x
+removeGarbage str = removeGarbage' False str
   where removeGarbage' _ [] = []
         removeGarbage' False ('<':xs) = removeGarbage' True xs
-        removeGarbage' False (x:xs) = [x] ++ removeGarbage' False xs
+        removeGarbage' False (x:xs) = x:(removeGarbage' False xs)
         removeGarbage' True ('>':xs) = removeGarbage' False xs
         removeGarbage' True (x:xs) = removeGarbage' True xs
 
@@ -21,8 +21,19 @@ score x = score' 0 x
         score' depth ('{':xs) = score' (depth + 1) xs
         score' depth ('}':xs) = depth + (score' (depth - 1) xs)
 
+removeNonGarbage :: [Char] -> [Char]
+removeNonGarbage str = removeNonGarbage' False str
+  where removeNonGarbage' _ [] = []
+        removeNonGarbage' False ('<':xs) = removeNonGarbage' True xs
+        removeNonGarbage' False (_:xs) = removeNonGarbage' False xs
+        removeNonGarbage' True ('>':xs) = removeNonGarbage' False xs
+        removeNonGarbage' True (x:xs) = x:(removeNonGarbage' True xs)
+
 main = do
   input <- head <$> lines <$> getContents
 
   -- Part A
   print $ score $ removeBoringCharacters $ removeGarbage $ removeIgnoredCharacters input
+
+  -- Part B
+  print $ length $ removeNonGarbage $ removeIgnoredCharacters input
