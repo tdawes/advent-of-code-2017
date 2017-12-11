96bb09d1caa691462640292a1a7d10abb6cd8d66
Tom Dawes <tomdawes91@gmail.com>
2017-12-11 11:40:40 +0000

    2017: Day 11 (a)

---
+++ src/11.hs
@@ -0,0 +1,26 @@
+import Data.String.Utils
+
+horizontalDistance :: [[Char]] -> Int
+horizontalDistance dirs = abs $ sum $ map horiz dirs
+  where horiz (_:[]) = 0
+        horiz (_:'w':[]) = -1
+        horiz (_:'e':[]) = 1
+
+verticalDistance :: [[Char]] -> Int
+verticalDistance dirs = abs $ sum $ map vert dirs
+  where vert ('n':[]) = 2
+        vert ('s':[]) = -2
+        vert ('n':_) = 1
+        vert ('s':_) = -1
+
+distance :: [[Char]] -> Int
+distance dirs | v > h = (floor $ (0.5 * (fromIntegral (v - h)))) + h
+              | otherwise = h
+  where v = verticalDistance dirs
+        h = horizontalDistance dirs
+
+main = do
+  input <- split "," <$> head <$> lines <$> getContents
+
+  -- Part A
+  print $ distance input
