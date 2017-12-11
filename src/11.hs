7ab84a11ed79b6153f552adb5e4d9fcb669912c6
Tom Dawes <tomdawes91@gmail.com>
2017-12-11 12:30:54 +0000

    2017: Day 11 (2nd attempt)

--- src/11.hs
+++ src/11.hs
@@ -1,25 +1,30 @@
 import Data.String.Utils
 import Data.List (inits)
 
-horizontalDistance :: [[Char]] -> Int
-horizontalDistance dirs = abs $ sum $ map horiz dirs
-  where horiz (_:[]) = 0
-        horiz (_:'w':[]) = -1
-        horiz (_:'e':[]) = 1
+sum3 :: (Num a) => [(a, a, a)] -> (a, a, a)
+sum3 [] = (0, 0, 0)
+sum3 ((x,y,z):cs) = (x + xs, y + ys, z +zs)
+  where (xs, ys, zs) = sum3 cs
 
-verticalDistance :: [[Char]] -> Int
-verticalDistance dirs = abs $ sum $ map vert dirs
-  where vert ('n':[]) = 2
-        vert ('s':[]) = -2
-        vert ('n':_) = 1
-        vert ('s':_) = -1
+toHex :: [Char] -> (Int, Int, Int)
+toHex "n" = (1, 1, 0)
+toHex "s" = (-1, -1, 0)
+toHex "ne" = (0, 1, 1)
+toHex "nw" = (1, 0, -1)
+toHex "sw" = (0, -1, -1)
+toHex "se" = (-1, 0, 1)
+
+dist :: (Int, Int, Int) -> Int
+dist (x, y, z) =  ((abs x) + (abs y) + (abs z)) `quot` 2
 
 distance :: [[Char]] -> Int
-distance dirs | v > h = (floor $ (0.5 * (fromIntegral (v - h)))) + h
-              | otherwise = h
-  where v = verticalDistance dirs
-        h = horizontalDistance dirs
+distance dirs = dist $ sum3 $ map toHex dirs
 
+maxDistance :: [[Char]] -> Int
+maxDistance dirs = maxDistance (0, 0, 0) dirs
+  where maxDistance pos [] = dist pos
+        maxDistance pos (d:ds) = max (dist newPos) (maxDistance newPos ds)
+          where newPos = sum3 [pos, toHex d]
 main = do
   input <- split "," <$> head <$> lines <$> getContents
 
@@ -27,4 +32,4 @@ main = do
   print $ distance input
 
   -- Part B
-  print $ maximum $ map distance $ inits input
+  print $ maxDistance input
