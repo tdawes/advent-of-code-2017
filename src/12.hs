6d2bd744ff06f6e6d77a3a5c4cd66cdb35d53127
Tom Dawes <tomdawes91@gmail.com>
2017-12-12 14:21:53 +0000

    2017: Day 12 (b)

--- src/12.hs
+++ src/12.hs
@@ -17,8 +17,17 @@ findGroup x nodes = findGroup' nodes Set.empty x
                                    | otherwise= Set.insert x $ Set.unions $ map (findGroup' nodes newVisited) (neighbours x)
           where neighbours x = snd $ fromJust $ find (\node -> fst node == x) nodes
                 newVisited = Set.insert x visited
+
+groups :: [Node] -> [Set.Set Int]
+groups [] = []
+groups ns = newGroup:(groups [n | n <- ns, not $ Set.member (fst n) newGroup])
+  where newGroup = findGroup (fst $ head ns) ns
+
 main = do
   input <- map (parseInput . words) <$> lines <$> getContents
 
   -- Part A
   print $ Set.size $ findGroup 0 input
+
+  -- Part B
+  print $ length $ groups input
