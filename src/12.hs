281d426b029a28aa3d5ea716d437263043419905
Tom Dawes <tomdawes91@gmail.com>
2017-12-12 14:17:19 +0000

    2017: Day 12 (a)

---
+++ src/12.hs
@@ -0,0 +1,24 @@
+import Data.List (intercalate, find)
+import Data.String.Utils (split)
+import qualified Data.Set as Set
+import Data.Maybe (fromJust)
+
+type Node = (Int, [Int])
+
+parseInt :: [Char] -> Int
+parseInt x = read x :: Int
+
+parseInput :: [[Char]] -> Node
+parseInput (x:"<->":edges) = (parseInt x, map parseInt $ split "," $ intercalate "" edges )
+
+findGroup :: Int -> [Node] -> Set.Set Int
+findGroup x nodes = findGroup' nodes Set.empty x
+  where findGroup' nodes visited x | Set.member x visited = Set.empty
+                                   | otherwise= Set.insert x $ Set.unions $ map (findGroup' nodes newVisited) (neighbours x)
+          where neighbours x = snd $ fromJust $ find (\node -> fst node == x) nodes
+                newVisited = Set.insert x visited
+main = do
+  input <- map (parseInput . words) <$> lines <$> getContents
+
+  -- Part A
+  print $ Set.size $ findGroup 0 input
