6072481fa29f544c781ccf97bb5773dee077d109
Tom Dawes <tomdawes91@gmail.com>
2017-12-13 12:24:01 +0000

    2017: Day 13 (b)

--- src/13.hs
+++ src/13.hs
@@ -13,12 +13,18 @@ position t r | p < r = p
              | otherwise = (2 * (r - 1)) - p
   where p = t `mod` (2 * (r - 1))
 
-severity :: Wall -> Int
-severity (d, r) | position d r == 0 = (d * r)
-                | otherwise = 0
+caught :: Int -> Wall -> Bool
+caught delay (d, r) = position (d + delay) r == 0
+
+severity :: Int -> Wall -> Int
+severity delay (d, r) | caught delay (d, r) = (d * r)
+                      | otherwise = 0
 
 main = do
   input <- map (parseInput . words) <$> lines <$> getContents
 
   -- Part A
-  print $ sum $ map severity input
+  print $ sum $ map (severity 0) input
+
+  -- Part B
+  print $ head [ t | t <- [0..], all (\w -> not $ caught t w) input ]
