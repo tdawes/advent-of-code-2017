f823669fbd7b0555d2b0e21ba9874575f9d8e174
Tom Dawes <tomdawes91@gmail.com>
2017-12-17 19:31:02 +0000

    2017: Day 17 (b)

--- src/17.hs
+++ src/17.hs
@@ -1,4 +1,9 @@
 import Data.List (elemIndex)
+import Data.Maybe (fromJust)
+import Control.Monad.ST
+import Data.STRef
+import Control.Monad.Loops
+import Control.Monad
 
 parseInt :: [Char] -> Int
 parseInt x = read x :: Int
@@ -12,9 +17,25 @@ spinlock turns step = spinlock' [0] turns step
   where spinlock' buffer 0 _ = buffer
         spinlock' buffer t s = spinlock' ((turns + 1 - t):(twist (s+1) buffer)) (t-1) s
 
+afterZero :: Int -> Int -> Int
+afterZero turns step = runST $ do
+  currentPos <- newSTRef 0
+  afterZ <- newSTRef 0
+
+  forM_ [1..turns] $ \x -> do
+    pos <- readSTRef currentPos
+    let newPos = (pos + step) `mod` x
+    writeSTRef currentPos (newPos + 1)
+    when (newPos == 0) $ do
+      writeSTRef afterZ x
+
+  readSTRef afterZ
 
 main = do
   input <- parseInt <$> head <$> lines <$> getContents
 
   -- Part A
   print $ (spinlock 2017 input) !! 1
+
+  -- Part B
+  print $ afterZero 50000000 input
