35fe8338f51409bc510014b740f07e3adf74039f
Tom Dawes <tomdawes91@gmail.com>
2017-12-20 11:52:45 +0000

    2017: Day 20 (b)

--- src/20.hs
+++ src/20.hs
@@ -1,5 +1,8 @@
 import Data.String.Utils (split)
 import Data.List (minimumBy)
+import qualified Data.Set as S
+import Data.List.Unique (isUnique)
+import Data.Maybe (fromJust)
 
 type Coordinate = (Int, Int, Int)
 data Data = Data { index :: Int
@@ -22,8 +25,25 @@ parseInput (index, line) = Data { index = index, position = (parseCoordinate $ p
 compareCoords :: Coordinate -> Coordinate -> Ordering
 compareCoords (a, b, c) (x, y, z) = compare ((abs a) + (abs b) + (abs c)) ((abs x) + (abs y) + (abs z))
 
+coordSum :: Coordinate -> Coordinate -> Coordinate
+coordSum (a, b, c) (x, y, z) = (a + x, b + y, c + z)
+
+updateData :: Data -> Data
+updateData d = Data { index = (index d), position = newPosition, velocity = newVelocity, acceleration = newAcceleration }
+  where newAcceleration = acceleration d
+        newVelocity = coordSum (velocity d) newAcceleration
+        newPosition = coordSum (position d) newVelocity
+
+update :: [Data] -> [Data]
+update datas = [ d | d <- newDatas, fromJust $ isUnique (position d) newPositions ]
+  where newDatas = map updateData datas
+        newPositions = map position newDatas
+
 main = do
   input <- map parseInput <$> zip [0..] <$> lines <$> getContents
 
   -- Part A
   print $ index $ minimumBy (\x y -> compareCoords (acceleration x) (acceleration y) ) input
+
+  -- Part B
+  print $ length $ (!!10000) $ iterate update input
